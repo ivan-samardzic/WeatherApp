fcd07bd51482c903fac80568d7e7478c43a93f88
Ivan Samardzic <isamar01@fesb.hr>
2021-03-20 11:37:04 +0100

    Add Header and Footer components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.scss'
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.scss'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Header
