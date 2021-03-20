fe6c53030f536ea208fcb1d397549147659d4955
Ivan Samardzic <isamar01@fesb.hr>
2021-03-20 12:49:47 +0100

    Change Header and Footer compos, nest all child components inside them

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,15 @@
 import './Footer.scss'
 import React from 'react'
+import LogoBox from '../LogoBox/LogoBox'
+import CopyrightBox from '../CopyrightBox/CopyrightBox'
+import SocialMediaBox from '../SocialMediaBox/SocialMediaBox'
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='footer-box'>
+            <LogoBox />
+            <CopyrightBox />
+            <SocialMediaBox />
         </div>
     )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import './Header.scss'
 import React from 'react'
-
+import HeaderLogo from '../HeaderLogo/HeaderLogo'
+import SearchBox from '../SearchBox/SearchBox'
 const Header = () => {
     return (
-        <div>
-            
+        <div className='header-box'>
+            <HeaderLogo />
+            <SearchBox />
         </div>
     )
 }
