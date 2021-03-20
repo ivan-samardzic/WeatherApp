d2232617372a21d1774ffe6e23829aca2d1c7427
Ivan Samardzic <isamar01@fesb.hr>
2021-03-20 12:46:17 +0100

    Add compo to design the header

---
+++ src/components/CirclesBox/CirclesBox.js
@@ -0,0 +1,14 @@
+import './CirclesBox.scss'
+import React from 'react'
+
+const CirclesBox = () => {
+    return (
+        <div className='circles-box'>
+            <i className="fas fa-circle red"></i>
+            <i className="fas fa-circle yellow"></i>
+            <i className="fas fa-circle green"></i>
+        </div>
+    )
+}
+
+export default CirclesBox
