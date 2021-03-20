19a5ad4fb95b8ebde02051642184fde19a759859
Ivan Samardzic <isamar01@fesb.hr>
2021-03-20 12:46:43 +0100

    Add compo to render the app logo

---
+++ src/components/LogoBox/LogoBox.js
@@ -0,0 +1,13 @@
+import './LogoBox.scss'
+import React from 'react'
+
+const LogoBox = () => {
+    return (
+        <div className='logo-box'>
+            <h3>WeatherApp</h3>
+            <h4>Only Actual Data</h4>
+        </div>
+    )
+}
+
+export default LogoBox
