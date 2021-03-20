b1cd5660e77ff3c4e9c059c0610d00379f2ddffb
Ivan Samardzic <isamar01@fesb.hr>
2021-03-20 13:45:49 +0100

    Add new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.scss';
+import React, { useState } from 'react'
 import Header from './components/Header/Header'
+import MainView from './components/MainView/MainView'
+import InitialView from './components/InitialView/InitialView'
 import Footer from './components/Footer/Footer'
 
 const App = () => {
+  const [loading, setLoading] = useState(false)
   return (
     <div className='container-fluid'>
       <Header />
+      {loading ? <InitialView /> : <MainView />}
       <Footer />
     </div>
   );
