96ef406952bf1a787bd3fd5cd6b875574ebc1a8e
Ivan Samardzic <isamar01@fesb.hr>
2021-03-21 20:51:03 +0100

    Add compo to render cities in the list

--- src/components/ListBox/ListBox.js
+++ src/components/ListBox/ListBox.js
@@ -1,10 +1,15 @@
 import './ListBox.scss'
 import React from 'react'
 
-const ListBox = () => {
+const ListBox = ({cities}) => {
     return (
-        <div>
-            
+        <div className='list-box'>
+            {cities.map((city, index) => 
+                <div className='list-item'>
+                    <div className='city-name'>{city.name}</div>
+                    <div className='city-temp'>{Math.round(city.main.temp)}<span>&deg;C</span></div>
+                </div>
+            )}
         </div>
     )
 }
