b67b848035d57e65d020a1172a2b8d02dca1246e
Ivan Samardzic <isamar01@fesb.hr>
2021-03-22 19:17:26 +0100

    Change compo which renders all favorite cities

--- src/components/ListBox/ListBox.js
+++ src/components/ListBox/ListBox.js
@@ -1,15 +1,31 @@
 import './ListBox.scss'
-import React from 'react'
+import React, {useState, useEffect, useContext} from 'react'
+import WeatherContext from '../../context/weatherContext'
+import RemoveFavorite from '../RemoveFavorite/RemoveFavorite'
 
-const ListBox = ({cities}) => {
+const ListBox = () => {
+    const context = useContext(WeatherContext)
+    const {favorites, deleteCity, setCurrentCity} = context;
+    console.log(favorites)
     return (
         <div className='list-box'>
-            {cities.map((city, index) => 
-                <div className='list-item'>
-                    <div className='city-name'>{city.name}</div>
-                    <div className='city-temp'>{Math.round(city.main.temp)}<span>&deg;C</span></div>
+            <div className='list-box-name'>Favorites</div>
+            {favorites.length > 0 ?
+                favorites.map((favorite, index) => 
+                <div className="list-item m-3">
+                    <div className="list-item-name" onClick={() => setCurrentCity(favorite)}>
+                        {favorite.name}
+                    </div>
+                    <div className="list-item-temp">
+                        {Math.round(favorite.main.temp)}<span>&deg;C</span>
+                    </div>
+                    <div onClick={() => deleteCity(favorite)}>
+                        <RemoveFavorite />
+                    </div>
                 </div>
-            )}
+                ) :
+                <p style={{color : "white", fontSize : "16px"}}>No Cities In Favorites List</p>
+                }
         </div>
     )
 }
