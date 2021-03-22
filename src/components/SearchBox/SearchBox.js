ee761c395e20448194cf8b3d9da7631080804782
Ivan Samardzic <isamar01@fesb.hr>
2021-03-22 19:20:37 +0100

    Change compo which store user input

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,11 +1,22 @@
 import './SearchBox.scss'
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import WeatherContext from '../../context/weatherContext'
 
 const SearchBox = () => {
+    const [name, setName] = useState('')
+    const context = useContext(WeatherContext);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        context.addCityNameToUrl(name);
+        setName('');
+    }
     return (
-        <form className="form">
+        <form className="form" onSubmit={(event) => handleSubmit(event)}>
             <input type="text" className="form-control"
             placeholder="Search City..." 
+            value={name} onChange={(event) => setName(event.target.value)}
             />
             <button type="submit" className="btn"></button>
             <i className="fas fa-search"></i>
