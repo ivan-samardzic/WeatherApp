81f9f467853b3c7d260c1ad928fff0b31834cc84
Ivan Samardzic <isamar01@fesb.hr>
2021-03-22 19:10:29 +0100

    Add components to add and delete cities from favorites

---
+++ src/components/AddFavorite/AddFavorite.js
@@ -0,0 +1,12 @@
+import './AddFavorite.scss'
+import React from 'react'
+
+const AddFavorite = () => {
+    return (
+        <div className="add-favorite">
+            <i className="fas fa-heart"></i>
+        </div>
+    )
+}
+
+export default AddFavorite
---
+++ src/components/RemoveFavorite/RemoveFavorite.js
@@ -0,0 +1,13 @@
+import './RemoveFavorite.scss'
+import React from 'react'
+
+const RemoveFavorite = () => {
+    return (
+        <div className="remove-favorite">
+            <i className="far fa-trash-alt">
+            </i>
+        </div>
+    )
+}
+
+export default RemoveFavorite
