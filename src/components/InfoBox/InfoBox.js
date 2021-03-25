7acb659dd8bf303ad932d2f7d44afca1ccadeabe
Ivan Samardzic <isamar01@fesb.hr>
2021-03-25 20:41:02 +0100

    Add minor changes to compo design

--- src/components/InfoBox/InfoBox.js
+++ src/components/InfoBox/InfoBox.js
@@ -6,7 +6,7 @@ const InfoBox = () => {
     return (
         <div className="message-box">
             <img src={PleadingFace} alt="pleading-face" />
-            <h3>Currently you dont have any locations</h3>
+            <h3>Currently you dont have any locations!</h3>
             <h4>It is easy! You can add your city easily just by clicking Burger Button and typing your city in or directly in the search box!</h4>
         </div>
     )
