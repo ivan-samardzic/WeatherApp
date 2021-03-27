65f8f77f1791ee0fd541f480a8a9295181d49744
Ivan Samardzic <isamar01@fesb.hr>
2021-03-27 19:52:26 +0100

    Change About page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            
+            <p>About This App, Return to the Homepage, this isn't correct page URL.</p>
         </div>
     )
 }
